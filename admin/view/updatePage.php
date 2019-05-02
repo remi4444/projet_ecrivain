925d27f4f1e7a9dcb42ddf998edcac2d8323599b
Rem44 <remi.puvilland@gmail.com>
2019-05-02 12:20:33 +0200

    Dans updatePage de l'admin modification de la mise en page comme adminPage

--- admin/view/updatePage.php
+++ admin/view/updatePage.php
@@ -3,8 +3,9 @@
 foreach($chapterElement as $chapter)
     {?>
 <form action="indexAdmin.php?action=updateChapter&amp;id=<?=$chapter->getId()?>" method = "post" class="jumbotron ">
-            <input type = "text" name = "chapter_number" value = "<?= $chapter->getChapterNumber()?>">
-            <input type="text" name="title" value ="<?= $chapter->getTitle()?>">
+            <label>Entrez le numéro de chapitre : </label><input type = "text" name = "chapter_number" value = "<?= $chapter->getChapterNumber()?>">
+            <label>Entrez le titre du chapitre : </label><input type="text" name="title" value ="<?= $chapter->getTitle()?>">
+            <p>Editez votre texte : </p>
             <textarea name = "text_paragraph" id="text_paragraph"><?= $chapter->getParagraph()?></textarea><br><br>
             <input type="submit" value ="Modifier le texte" class="text-danger"> 
 </form>
